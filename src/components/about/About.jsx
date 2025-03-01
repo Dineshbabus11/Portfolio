5b0e705bec36e392675fce3abfbedc12381d8db3
Dineshbabus11 <msgtodineshbabus@gmail.com>
2025-03-01 20:22:39 +0530

    Update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,7 +11,7 @@ export const About = () => {
             <InfoIcon/>
         </div>
         <div className='about_content'>
-            <p>I'm Dinesh Babu S, a B.Tech IT student at KPR Institute of Engineering and Technology (3rd year), specializing in Java development, React.js, and web development. I have hands-on experience in branding and social media campaign management, helping businesses grow digitally.<br/>During my internship at Thulir, I worked on a Hotel Management System, enhancing my problem-solving and teamwork skills. Passionate about technology and digital solutions, I aim to build seamless and efficient applications.</p>
+            <p>I'm Dinesh Babu S, a B.Tech IT student at KPR Institute of Engineering and Technology (3rd year), specializing in Java development, React.js, and web development. I have hands-on experience in branding and social media campaign management, helping businesses grow digitally.<br/><br/>During my internship at Thulir, I worked on a Hotel Management System, enhancing my problem-solving and teamwork skills. Passionate about technology and digital solutions, I aim to build seamless and efficient applications.</p>
             <img src={profilepic} alt="Dinesh Babu S" />
         </div>
     </div>
